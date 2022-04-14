6453b5322335f1bbd39415c8c9e7be0ee8af32a4
willi <willi@NOTEBOOKDELL>
2022-04-14 14:42:15 -0300

    Adicionar arquivos de projeto.

---
+++ src/DevIO.API/Controllers/FornecedoresController.cs
@@ -0,0 +1,99 @@
+﻿using AutoMapper;
+using DevIO.API.ViewModels;
+using DevIO.Business.Intefaces;
+using DevIO.Business.Models;
+using DevIO.Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.API.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IFornecedorService _fornecedorService;
+        private readonly IMapper _mapper;
+ 
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper, IFornecedorService fornecedorService)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+            _fornecedorService = fornecedorService;
+
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+
+            var listaFornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return listaFornecedores;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
+        {
+
+            var fornecedor = _mapper.Map<FornecedorViewModel>(await _fornecedorRepository.ObterPorId(id));
+
+            if (fornecedor == null) return NotFound();
+
+            return fornecedor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> Adicionar(FornecedorViewModel fornecedorViewModel) 
+        {
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+
+            var retorno = await _fornecedorService.Adicionar(fornecedor);
+
+            if (!retorno) return BadRequest();
+
+            return Ok();
+        
+        }
+
+        [HttpPost("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Atualizar(Guid id, FornecedorViewModel fornecedorViewModel)
+        {
+            if (id != fornecedorViewModel.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+
+            var retorno = await _fornecedorService.Atualizar(fornecedor);
+
+            if (!retorno) return BadRequest();
+
+            return Ok();
+
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> Excluir(Guid id) 
+        {
+            var fornecedor = await _fornecedorRepository.ObterPorId(id);
+            var fornecedorViewModel = _mapper.Map<FornecedorViewModel>(fornecedor);
+
+            if (fornecedor == null) return NotFound();
+
+            var resultado = await _fornecedorService.Remover(id);
+
+            if (!resultado) return BadRequest();
+
+            return Ok(fornecedorViewModel);
+
+
+        }
+
+    }
+}
---
+++ src/DevIO.API/Controllers/MainController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.API.Controllers
+{
+    public abstract class MainController : ControllerBase
+    {
+        // validação de notificações de erro
+
+        // validação de model state
+
+        // validação da operação  de negocios
+
+
+    }
+}
